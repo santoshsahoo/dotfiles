d29318df0af453ba9652217edacaef47fa957ae6
santoshsahoo <me@santoshsahoo.com>
2014-09-30 20:16:22 +0000

    Added pydata

---
+++ pydata.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib as mpl
+mpl.use('agg')
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+from pprint import pprint as pp
+print('imported numpy as np, matplotlib as mpl, pyplot as plt and pandas as pd')
